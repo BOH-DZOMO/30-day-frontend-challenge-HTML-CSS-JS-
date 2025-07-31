a225f07f284f7c830fb6caf48174da3d7c59f31f
BOH-DZOMO <bohdzomobobpeter@gmail.com>
2025-07-31 23:57:50 +0100

    solved the eventlistener bug for day-5 project

--- day-5/main.js
+++ day-5/main.js
@@ -1,29 +1,35 @@
-let lists = document.getElementsByClassName("list")
-let rightBox = document.getElementById("right")
-let leftBox = document.getElementById("left")
+//2 notes
+// don't nest event listeners
+// always use if statement to verify content or state of global varibles or rresources initiated somewhere else
+//To do
+// you can use DataTransfer and learn more about drag and drop API events
 
-console.log(lists);
+let lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
 
 
-
+let selected = null;
 for (const list of lists) {
-    list.addEventListener("dragstart",(e)=>{
-        let selected = e.target;
-        rightBox.addEventListener("dragover",(e)=>{
-            e.preventDefault();
-        })
-        rightBox.addEventListener("drop",(e)=>{
-            rightBox.appendChild(selected)
-            selected = null
-        })
-        leftBox.addEventListener("dragover",(e)=>{
-            e.preventDefault();
-        })
-        leftBox.addEventListener("drop",(e)=>{
-            leftBox.appendChild(selected)
-            selected = null
-        })
-    })
-    
-    
+  list.addEventListener("dragstart", (e) => {
+    selected = e.target;
+  });
+  rightBox.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  rightBox.addEventListener("drop", (e) => {
+   if (selected) {
+    rightBox.appendChild(selected);
+    selected = null;
+   }
+  });
+  leftBox.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  leftBox.addEventListener("drop", (e) => {
+    if (selected) {
+        leftBox.appendChild(selected);
+    selected = null;
+    }
+  });
 }
